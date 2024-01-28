37672a3f038ba2a1de6dc87644fa6d29941f29c2
Julio Cesar <contato.jcsalerno@gmail.com>
2024-01-28 19:17:21 -0300

    feat: created method delete post

--- src/apps/controllers/PostController.js
+++ src/apps/controllers/PostController.js
@@ -15,5 +15,32 @@ class PostController {
     }
     return res.status(200).json({ post: newPost });
   }
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const verifyPost = await Posts.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!verifyPost) {
+      return res.status(404).json({ message: "Post does not exits!" });
+    }
+    if (verifyPost.author_id !== req.userId) {
+      return res
+        .status(401)
+        .json({ message: "You dont have permisson to delete this post" });
+    }
+    const deletedPost = await Posts.destroy({
+      where: {
+        id,
+      },
+    });
+    if (!deletedPost) {
+      return res.status(400).json({ message: "Failed to delete this post!" });
+    }
+    return res.status(200).json({ message: "Post deleted!" });
+  }
 }
 module.exports = new PostController();
--- src/routes.js
+++ src/routes.js
@@ -30,5 +30,6 @@ routes.get("/user-profile", UserController.userProfile);
 routes.post("/upload", upload.single("image"), FileController.upload);
 
 routes.post("/new-post", schemaValidator(postSchema), PostController.create);
+routes.delete("/delete-post/:id", PostController.delete);
 
 module.exports = routes;
